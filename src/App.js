f1d697e7e6610b298c88ab0b70da8458111f71ce
Rohit171994 <rohitkumarpoddar1917@gmail.com>
2022-06-15 13:58:59 +0530

    added lazyloading

--- src/App.js
+++ src/App.js
@@ -3,23 +3,32 @@ import "./App.css";
 import Header from "./components/Header";
 import AppRoutes from "./Routes/Route";
 import Aside from "./components/Aside";
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <div class="row">
-          <div class="col-sm-12" style={{backgroundColor:'skyblue'}}>
-            <Header />
+      <React.Suspense fallback={<span>Loading...</span>}>
+        <BrowserRouter>
+          <div class="row">
+            <div class="col-sm-12" style={{ backgroundColor: "skyblue" }}>
+              <Header />
+            </div>
+            <div
+              class="col-sm-4"
+              style={{ backgroundColor: "lightsalmon", height: "85vh" }}
+            >
+              <Aside />
+            </div>
+            <div
+              class="col-sm-8"
+              style={{ backgroundColor: "lightgrey", height: "85vh" }}
+            >
+              <AppRoutes />
+            </div>
           </div>
-          <div class="col-sm-4" style={{backgroundColor:'lightsalmon', height:'85vh'}}>
-            <Aside />
-          </div>
-          <div class="col-sm-8" style={{backgroundColor:'lightgrey',height:'85vh'}}>
-            <AppRoutes />
-          </div>
-        </div>
-      </BrowserRouter>
+        </BrowserRouter>
+      </React.Suspense>
     </div>
   );
 }
--- src/Routes/Route.js
+++ src/Routes/Route.js
@@ -1,15 +1,17 @@
+// Static importing
 import React from "react";
-import {Switch, Route} from 'react-router-dom'
-import Home from "../components/Home";
-import AboutUs from "../components/AboutUs";
+import { Switch, Route } from "react-router-dom";
+// Dynamic importing
+const Home = React.lazy(() => import("../components/Home"));
+const ABoutUs = React.lazy(() => import("../components/AboutUs"));
 
-const AppRoutes = () =>{
-    return(
+const AppRoutes = () => {
+  return (
     <Switch>
-        <Route exact path ='/' component = {Home}/>
-        <Route  path ='/aboutus' component = {AboutUs}/>
+      <Route exact path="/" render={() => <Home />} />
+      <Route path="/aboutus" render={() => <ABoutUs />} />
     </Switch>
-    )
-}
+  );
+};
 
 export default AppRoutes;
